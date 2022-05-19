09812dd79933547e1f738341a083505cae4a3d5f
Afonso-Boto <afonsomsboto@gmail.com>
2022-05-19 15:49:00 +0100

    Project Setup , Laundry

---
+++ LaundryAtHome/LaundryPlatform/src/main/java/tqs/project/laundryplatform/LaundryPlatformApplication.java
@@ -0,0 +1,13 @@
+package tqs.project.laundryplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaundryPlatformApplication.class, args);
+    }
+
+}
