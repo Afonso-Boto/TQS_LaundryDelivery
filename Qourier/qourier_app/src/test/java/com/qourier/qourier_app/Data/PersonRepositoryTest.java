2470c0ff71b302a9998ced9380ba4904c2b95174
Pengrey <rodrigoflima.me@gmail.com>
2022-05-28 18:01:03 +0100

    Clean

--- Qourier/qourier_app/src/test/java/com/qourier/qourier_app/Data/PersonRepositoryTest.java
+++ Qourier/qourier_app/src/test/java/com/qourier/qourier_app/Data/PersonRepositoryTest.java
@@ -2,7 +2,6 @@ package com.qourier.qourier_app.Data;
 
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
