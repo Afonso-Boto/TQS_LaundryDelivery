90920d07dab4f02b4647b21b1fe2d9dc083346f0
Martinho Tavares <martinho.tavares@ua.pt>
2022-05-30 10:03:45 +0100

    Testing Google Java formatting with import order fixing

--- Qourier/qourier_app/src/main/java/com/qourier/qourier_app/TestMe.java
+++ Qourier/qourier_app/src/main/java/com/qourier/qourier_app/TestMe.java
@@ -1,10 +1,17 @@
 package com.qourier.qourier_app;
 
+import java.util.random.RandomGenerator;
+
+import static java.util.Map.of;
+import java.util.Map;
+
 public class TestMe {
 
-  static public void main(String[] args) {
-                String AasgapSAPGOaNg;
-    if (true)
-    {
-      String oa = "";}}
+    public static void main(String[] args) {
+        String AasgapSAPGOaNg;
+        if (true) {
+            String oa = "";
+        }
+        Map<String, String> a = of("s", "s");
+    }
 }
