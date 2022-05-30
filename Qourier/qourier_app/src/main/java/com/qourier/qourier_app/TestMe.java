373e4e198b5e1675f27c3fc95c2e95c21ddeda1e
Martinho Tavares <martinho.tavares@ua.pt>
2022-05-30 09:30:56 +0100

    Testing Google Java formatting

---
+++ Qourier/qourier_app/src/main/java/com/qourier/qourier_app/TestMe.java
@@ -0,0 +1,9 @@
+package com.qourier.qourier_app;
+
+public class TestMe {
+
+          public static void main(String[] args) {
+    String AasgapSAPGOaNg;
+    if (true) {String oa = "";}
+              }
+}
