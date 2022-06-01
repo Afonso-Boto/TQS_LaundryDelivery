fc6033d40cb469707c4431252a39e3cfde46a0c7
Martinho Tavares <martinho.tavares@ua.pt>
2022-06-01 16:59:56 +0100

    [TQS-70] Removed Failsafe plugin since it wasn't working properly

--- Qourier/qourier_app/src/test/java/com/qourier/qourier_app/WebOperationsIT.java
+++
@@ -1,4 +0,0 @@
-package com.qourier.qourier_app;
-
-public class WebOperationsIT {
-}
---
+++ Qourier/qourier_app/src/test/java/com/qourier/qourier_app/WebOperationsIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.qourier.qourier_app;
+
+import org.junit.jupiter.api.Test;
+
+class WebOperationsIntegrationTest {
+
+    @Test
+    void poggers() {
+        assert false;
+    }
+
+}
