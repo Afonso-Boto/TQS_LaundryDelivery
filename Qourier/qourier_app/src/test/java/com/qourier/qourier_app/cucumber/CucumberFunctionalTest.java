edf10dc906f3d1ea58c8e547d9cc0bd1fe5b4b32
Pengrey <rodrigoflima.me@gmail.com>
2022-06-02 15:35:07 +0100

    added in memory db for tests in selenium

--- Qourier/qourier_app/src/test/java/com/qourier/qourier_app/cucumber/CucumberFunctionalTest.java
+++ Qourier/qourier_app/src/test/java/com/qourier/qourier_app/cucumber/CucumberFunctionalTest.java
@@ -4,11 +4,6 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
@@ -16,21 +11,5 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com.qourier.qourier_app.cucumber")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.qourier.qourier_app.cucumber")
-@Testcontainers
-public class CucumberFunctionalTest {
-
-    @Container
-    public static MySQLContainer container = new MySQLContainer("mysql:8.0.29")
-            .withUsername("demo")
-            .withPassword("demopass")
-            .withDatabaseName("test_db");
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", container::getJdbcUrl);
-        registry.add("spring.datasource.password", container::getPassword);
-        registry.add("spring.datasource.username", container::getUsername);
-    }
-
-}
+public class CucumberFunctionalTest{}
 
