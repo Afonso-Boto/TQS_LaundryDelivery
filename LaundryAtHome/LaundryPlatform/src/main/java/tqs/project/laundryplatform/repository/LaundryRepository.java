49912cc2726d77dbfa20913d674c677b7492816c
Afonso-Boto <afonsomsboto@gmail.com>
2022-06-06 15:41:43 +0100

    [TQS-89] Added Repository

---
+++ LaundryAtHome/LaundryPlatform/src/main/java/tqs/project/laundryplatform/repository/LaundryRepository.java
@@ -0,0 +1,12 @@
+package tqs.project.laundryplatform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tqs.project.laundryplatform.model.Laundry;
+
+import java.util.Optional;
+
+@Repository
+public interface LaundryRepository extends JpaRepository<Laundry, Long> {
+    Optional<Laundry> findById(Long laundryId);
+}
