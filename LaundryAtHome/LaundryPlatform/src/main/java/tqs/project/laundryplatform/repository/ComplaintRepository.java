7291757ae8a80a2e25191df5624b1f88b2454d06
Afonso Boto <afonsomsboto@gmail.com>
2022-06-18 17:14:54 +0100

    [TQS-63] Added functions to Complaint repository

--- LaundryAtHome/LaundryPlatform/src/main/java/tqs/project/laundryplatform/repository/ComplaintRepository.java
+++ LaundryAtHome/LaundryPlatform/src/main/java/tqs/project/laundryplatform/repository/ComplaintRepository.java
@@ -3,4 +3,9 @@ package tqs.project.laundryplatform.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tqs.project.laundryplatform.model.Complaint;
 
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {}
+import java.util.Optional;
+
+public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+    Optional<Complaint> findById(Long complaintId);
+    Optional<Complaint> findByTitle(String title);
+}
