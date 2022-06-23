4bef95d8f003331b8b36810f6eb6b8685cc383a1
Pengrey <rodrigoflima.me@gmail.com>
2022-06-23 11:03:27 +0100

    [TQS-95] removed swagger

--- Qourier/qourier_app/src/main/java/com/qourier/qourier_app/QourierApp.java
+++ Qourier/qourier_app/src/main/java/com/qourier/qourier_app/QourierApp.java
@@ -9,18 +9,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class QourierApp {
 
     public static void main(String[] args) {
         SpringApplication.run(QourierApp.class, args);
-    }
-
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.qourier.qourier_app"))
-                .build();
     }
 }
