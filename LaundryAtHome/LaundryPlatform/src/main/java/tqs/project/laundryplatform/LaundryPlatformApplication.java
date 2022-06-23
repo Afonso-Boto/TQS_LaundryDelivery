92514354b14aabd9d9284a4e152a4a716375be21
Tomás Candeias <tomascandeias@ua.pt>
2022-06-23 02:13:35 +0100

    [TQS-96] swagger added

--- LaundryAtHome/LaundryPlatform/src/main/java/tqs/project/laundryplatform/LaundryPlatformApplication.java
+++ LaundryAtHome/LaundryPlatform/src/main/java/tqs/project/laundryplatform/LaundryPlatformApplication.java
@@ -5,8 +5,13 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LaundryPlatformApplication {
 
     public static void main(String[] args) {
@@ -16,5 +21,10 @@ public class LaundryPlatformApplication {
     @Bean
     public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
         return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("tqs.project.laundryplatform")).build();
     }
 }
